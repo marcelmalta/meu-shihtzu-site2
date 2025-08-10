8092392e0b36c2e0abe04a9b5e62ce48735a0b68
marcelmalta <marcelmalta@hotmail.com>
2025-08-10 10:46:53 -0300

    fix(build): usar @vitejs/plugin-react-swc no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 // vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     host: true,
+    port: 5173,
   },
   build: {
     outDir: "dist",
